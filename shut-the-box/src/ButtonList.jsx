10152288b8da24001000c91fa854dc73e1a4bc8c
uriziv17 <74453815+uriziv17@users.noreply.github.com>
2023-09-28 23:44:56 +0300

    button list working

--- shut-the-box/src/ButtonList.jsx
+++ shut-the-box/src/ButtonList.jsx
@@ -1,19 +1,55 @@
 import { useState } from 'react'
 
 
-const INITIALPOOL = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const INITIALPOOL = {
+    '1': true,
+    '2': true,
+    '3': true,
+    '4': true,
+    '5': true,
+    '6': true,
+    '7': true,
+    '8': true,
+    '9': true,
+}
 
+// const INITIALPOOL = new Map([
+//     [1, true],
+//     [2, true],
+//     [3, true],
+//     [4, true],
+//     [5, true],
+//     [6, true],
+//     [7, true],
+//     [8, true],
+//     [9, true]
+// ])
 
 export default function ButtonList() {
     const [numberPool, setNumberPool] = useState(INITIALPOOL)
 
-    const onClick = (e) => {
-        setNumberPool(numberPool.filter((n) => n != e.target.value))
+    const onClick = (n) => {
+        let newNums = { ...numberPool }
+        newNums[n] = false
+        setNumberPool(newNums)
+        // console.log(newNums)
     }
 
     return (
-        <div>
-            {numberPool.map((n) => <button onClick={onClick}>{n}</button>)}
+        <div className="button-list">
+            <div>
+                {Object.entries(numberPool).map((n) => {
+                    if (n[1]) {
+                        return (<button className="button active" onClick={() => onClick(n[0])}>{n[0]}</button>)
+                    }
+                    else {
+                        return (<button className="button disabled">{n[0]}</button>)
+                    }
+
+                })}
+            </div>
+            <button className='button active' onClick={() => setNumberPool(INITIALPOOL)}>RESET</button>
         </div>
+
     )
 }
